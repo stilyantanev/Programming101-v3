424110d98a443c10422678fd8ecffc619f755bc9
stilyantanev <stilyan.tanev@gmail.com>
2015-04-05 20:06:23 +0300

    Comment some tests

--- week3/3-Panda-Social-Network/panda_social_network.py
+++ week3/3-Panda-Social-Network/panda_social_network.py
@@ -175,22 +175,22 @@ class PandaSocialNetwork:
             all_friends = []
 
 
-network = PandaSocialNetwork()
-ivo = Panda("Ivo", "ivo@pandamail.com", "male")
-rado = Panda("Rado", "rado@pandamail.com", "male")
-tony = Panda("Tony", "tony@pandamail.com", "female")
+# network = PandaSocialNetwork()
+# ivo = Panda("Ivo", "ivo@pandamail.com", "male")
+# rado = Panda("Rado", "rado@pandamail.com", "male")
+# tony = Panda("Tony", "tony@pandamail.com", "female")
 
-for panda in [ivo, rado, tony]:
-    network.add_panda(panda)
+# for panda in [ivo, rado, tony]:
+#     network.add_panda(panda)
 
-network.make_friends(ivo, rado)
-network.make_friends(rado, tony)
+# network.make_friends(ivo, rado)
+# network.make_friends(rado, tony)
 
-print(network.connection_level(ivo, rado) == 1)  # True
-print(network.connection_level(ivo, tony) == 2)  # True
+# print(network.connection_level(ivo, rado) == 1)  # True
+# print(network.connection_level(ivo, tony) == 2)  # True
 
-print(network.how_many_gender_in_network(1, rado, "female"))   # True
+# print(network.how_many_gender_in_network(1, rado, "female"))   # True
 
-network.save_social_network("save_network.txt")
-network.load_social_network("save_network.txt")
-network.save_social_network("load_network.txt")
+# network.save_social_network("save_network.txt")
+# network.load_social_network("save_network.txt")
+# network.save_social_network("load_network.txt")
