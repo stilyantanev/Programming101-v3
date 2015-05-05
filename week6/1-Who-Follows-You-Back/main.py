12a1170b4ddb556c47f180653ea576286245e4c0
stilyantanev <stilyan.tanev@gmail.com>
2015-05-05 10:37:42 +0300

    Little fix

--- week6/1-Who-Follows-You-Back/main.py
+++ week6/1-Who-Follows-You-Back/main.py
@@ -11,17 +11,23 @@ def main():
     filip_net = Network()
     filip_net.build_social_graph(1, filip)
     pprint(filip_net.graph.nodes)
+
     print()
+
     stilyan_net = Network()
     stilyan_net.build_social_graph(1, stilyan)
     pprint(stilyan_net.graph.nodes)
+
     print()
+
     print(filip_net.do_you_follow(stilyan))
     print(filip_net.do_you_follow_indirectly(stilyan))
     print(filip_net.does_he_she_follows(stilyan))
     print(filip_net.do_you_follow_indirectly(stilyan))
     print(filip_net.who_follows_you_back())
+
     print()
+
     print(stilyan_net.do_you_follow(rado))
     print(stilyan_net.do_you_follow_indirectly(rado))
     print(stilyan_net.does_he_she_follows(rado))
